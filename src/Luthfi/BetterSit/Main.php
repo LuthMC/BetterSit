326a5a75a9a316dd5c76eafe644a06cd6a9ddc3c
LuthMC <luthfi366822@gmail.com>
2024-09-03 23:23:59 +0700

    Add Main.php

---
+++ src/Luthfi/BetterSit/Main.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Luthfi\BetterSit;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\Listener;
+use BetterSit\commands\SitToggleCommand;
+use BetterSit\listeners\EventListener;
+
+class Main extends PluginBase implements Listener {
+
+    private static $instance;
+    private $sitEnabled = [];
+
+    public static function getInstance(): Main {
+        return self::$instance;
+    }
+
+    public function onEnable(): void {
+        self::$instance = $this;
+        $this->getServer()->getPluginManager()->registerEvents(new EventListener($this), $this);
+        $this->getServer()->getCommandMap()->register("sittoggle", new SitToggleCommand($this));
+    }
+
+    public function isSitEnabled(string $playerName): bool {
+        return $this->sitEnabled[$playerName] ?? true;
+    }
+
+    public function toggleSit(string $playerName): void {
+        $this->sitEnabled[$playerName] = !$this->sitEnabled[$playerName];
+    }
+}
