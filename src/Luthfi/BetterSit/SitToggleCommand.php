56f36d993a9b40184ff55e2ae53a34ee57efb720
LuthMC <luthfi366822@gmail.com>
2024-09-03 23:27:16 +0700

    Add SitToggleCommand.php

---
+++ src/Luthfi/BetterSit/SitToggleCommand.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Luthfi\BetterSit;
+
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\player\Player;
+use Luthfi\BetterSit\Main;
+
+class SitToggleCommand extends Command {
+
+    private $plugin;
+
+    public function __construct(Main $plugin) {
+        parent::__construct("sittoggle", "Sit Toggle Command", "/sittoggle");
+        $this->plugin = $plugin;
+    }
+
+    public function execute(CommandSender $sender, string $label, array $args): void {
+        if ($sender instanceof Player) {
+            $this->plugin->toggleSit($sender->getName());
+            $status = $this->plugin->isSitEnabled($sender->getName()) ? "enabled" : "disabled";
+            $sender->sendMessage("Sitting is now " . $status);
+        } else {
+            $sender->sendMessage("This command can only be used by players.");
+        }
+    }
+}
